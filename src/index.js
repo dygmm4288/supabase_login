0d7fbfa5b315a8274d19097e7d024bd90f3fa840
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-12-16 21:56:45 +0900

    초기화

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import env from "dotenv";
-import process from "node";
-
-env.config();
-
-const supabase = createClient(process.env.SUPABASE_URL, process.env.ANON_KEY);
-
-export default supabase;
